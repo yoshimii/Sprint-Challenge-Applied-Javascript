5c57fba32ddfcabfd5c02e0ce7ba301883124d89
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-09 10:58:39 -0500

    created tabs with get request iterating through topics and appending children to parent

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,5 +11,17 @@ window.addEventListener('load', function(){
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+const tabDiv = document.querySelector('.topics')
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then((response)=> {
+    response.data.topics.forEach(topic => {//create div and append to parent for each element
+        let tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.textContent = topic;
+        tabDiv.appendChild(tab);
+    });
+})
+
 
 })
