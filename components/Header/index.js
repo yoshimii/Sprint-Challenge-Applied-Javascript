2b36fabe5ee5e3da08a755f3c9f8c5bb576b9a28
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-09 10:44:32 -0500

    created header elements

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,10 +11,23 @@ window.addEventListener('load', function() {
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
-
 const headerContainer = document.querySelector(".header-container");
 headerContainer.appendChild(Header());
 
 function Header() {}
+const deg = `\u00B0`;
+const header = document.createElement('div');
+header.classList.add('header');
+
+const date = document.createElement('span');
+date.classList.add('date');
+date.textContent = "March 28th, 2019";
+
+const title = document.createElement('h1');
+title.textContent = "Lambda Times";
+
+const weather = document.createElement('span');
+weather.classList.add('temp');
+weather.textContent = `98${deg}`;
 
 })
